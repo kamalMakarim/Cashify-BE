a1578a518a2bff29b54f7727f09ba7738429dd7c
kamalMakarim <kamal.makarim@gmail.com>
2024-11-26 16:19:23 +0700

    remove cors

--- cashify-be/index.js
+++ cashify-be/index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const userRoutes = require("./src/routes/user.routes");
 const trashRoutes = require('./src/routes/trash.routes');
 const collectionCenterRoutes = require('./src/routes/collection_center.routes');
-const cors = require('cors');
 
 require("dotenv").config();
 const port = process.env.PORT || 5000;
@@ -10,14 +9,6 @@ const port = process.env.PORT || 5000;
 const app = express();
 
 require("./src/config/mongo.config").connectDB();
-app.use(
-  cors({
-    origin: process.env.FE_URL,
-    credentials: true,
-    methods: 'GET,POST,PUT,DELETE,OPTIONS',
-    allowedHeaders: ['X-Requested-With', 'Content-Type', 'Authorization', 'Cookie']
-  })
-);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
