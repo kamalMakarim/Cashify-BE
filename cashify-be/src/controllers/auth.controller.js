d62f6b15c6efdec2c447abc834ca2f522c630bb4
kamalMakarim <kamal.makarim@gmail.com>
2024-11-26 19:20:19 +0700

    added domain

--- cashify-be/src/controllers/auth.controller.js
+++ cashify-be/src/controllers/auth.controller.js
@@ -25,6 +25,7 @@ exports.login = async (req, res) => {
       secure: process.env.NODE_ENV === "production",
       sameSite: process.env.NODE_ENV === "production" ? "Strict" : "Lax",
       maxAge: process.env.JWT_EXPIRES_IN * 60 * 60 * 1000,
+      domain: process.env.NODE_ENV === "production" ? ".eco-cashify.vercel.app" : undefined,
       path: "/",
     });
 
@@ -33,6 +34,7 @@ exports.login = async (req, res) => {
       secure: process.env.NODE_ENV === "production",
       sameSite: process.env.NODE_ENV === "production" ? "Strict" : "Lax",
       maxAge: process.env.JWT_EXPIRES_IN * 60 * 60 * 1000,
+      domain: process.env.NODE_ENV === "production" ? ".eco-cashify.vercel.app" : undefined,
       path: "/",
     });
 
--- cashify-be/src/controllers/trash.controller.js
+++ cashify-be/src/controllers/trash.controller.js
@@ -77,6 +77,7 @@ exports.claim = async (req, res) => {
       secure: process.env.NODE_ENV === "production",
       sameSite: process.env.NODE_ENV === "production" ? "Strict" : "Lax",
       maxAge: process.env.JWT_EXPIRES_IN * 60 * 60 * 1000,
+      domain: process.env.NODE_ENV === "production" ? ".eco-cashify.vercel.app" : undefined,
       path: "/",
     });
 
